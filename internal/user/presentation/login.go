99d42a75f348e5a61790f07121da1ce6ec347732
Julio Perdiguer <julio.perdiguer@zolar.de>
2024-09-20 18:03:14 +0200

    9 - tests done

--- internal/user/presentation/login.go
+++ internal/user/presentation/login.go
@@ -5,14 +5,24 @@ import (
 	"user-service/internal/user/application/login"
 )
 
+// LoginRequest represents the expected login payload
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+// LoginResponse represents the response payload for a successful login
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
 // LoginUserHandler is a handler to login a user
 // @Summary Authenticates a user with the provided email and password
 // @Schemes
 // @Description Authenticates a user and returns a JWT token if credentials are valid
-// @Accept application/x-www-form-urlencoded
+// @Accept  json
 // @Produce json
-// @Param email formData string true "User email" example(jlp@federation.com)
-// @Param password formData string true "User password" example(enterprise)
+// @Param   login body      login.LoginRequest  true  "Login request body"
 // @Success 200 {object} map[string]string "Token generated successfully" example({"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."})
 // @Failure 400 {object} map[string]string "Invalid input" example({"error": "invalid input"})
 // @Failure 401 {object} map[string]string "Invalid credentials" example({"error": "invalid credentials"})
@@ -21,15 +31,21 @@ import (
 // @Tags user
 func LoginUserHandler(service *login.UserLoginService) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		email := c.PostForm("email")
-		password := c.PostForm("password")
+		var req LoginRequest
 
-		token, err := service.Login(email, password)
+		// Bind JSON request body to struct
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(400, gin.H{"error": "invalid input"})
+			return
+		}
+
+		// Call login service with email and password
+		token, err := service.Login(req.Email, req.Password)
 		if err != nil {
 			c.JSON(err.Code, gin.H{"error": err.Error()})
 			return
 		}
 
-		c.JSON(200, gin.H{"token": token})
+		c.JSON(200, LoginResponse{Token: token})
 	}
 }
--- internal/user/presentation/login_test.go
+++ internal/user/presentation/login_test.go
@@ -1,16 +1,20 @@
 package presentation
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
 	"user-service/internal/user/application/login"
 	"user-service/internal/user/domain"
 	"user-service/internal/user/domain/domainmocks"
+	"user-service/kit"
 )
 
 func TestLoginUserHandler(t *testing.T) {
@@ -23,21 +27,90 @@ func TestLoginUserHandler(t *testing.T) {
 	userService := login.NewUserLoginService(repo, tokenService)
 	router.POST("/login", LoginUserHandler(userService))
 
+	uid, err := kit.NewUuidValueObject("6d0f12c8-9fe9-4506-ad59-d386adbbe5c0")
+	require.NoError(t, err)
+	email, err := kit.NewEmailValueObject("jlp@federation.com")
+
 	t.Run("Test correct email and password", func(t *testing.T) {
 
-		repo.On("FindByEmail", "jlp@federation.com").Return(&domain.User{}, nil)
+		mockUser, err := domain.NewUser(
+			uid,
+			"Jean Luc Picard",
+			email,
+			"enterprise",
+		)
+
+		repo.On("FindByEmail", "jlp@federation.com").Return(mockUser, nil)
 		tokenService.On("GenerateToken", mock.Anything).Return("token", nil)
 
-		payload := "email=jlp@federation.com&password=enterprise"
-		req, err := http.NewRequest("POST", "/login", strings.NewReader(payload))
+		payload := map[string]string{
+			"email":    "jlp@federation.com",
+			"password": "enterprise",
+		}
+
+		body, _ := json.Marshal(payload)
+
+		req, err := http.NewRequest("POST", "/login", bytes.NewBuffer(body))
 		require.NoError(t, err)
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		req.Header.Set("Content-Type", "application/json")
 
 		recorder := httptest.NewRecorder()
 		router.ServeHTTP(recorder, req)
 
 		require.Equal(t, http.StatusOK, recorder.Code)
 		tokenService.AssertExpectations(t)
+
+		var response LoginResponse
+		err = json.Unmarshal(recorder.Body.Bytes(), &response)
+		require.NoError(t, err)
+		assert.Equal(t, "token", response.Token)
 	})
 
+	t.Run("Test incorrect email", func(t *testing.T) {
+
+		repo.On("FindByEmail", "wr@federation.com").Return(nil, errors.New("some error"))
+
+		payload := map[string]string{
+			"email":    "wr@federation.com",
+			"password": "enterprise",
+		}
+
+		body, _ := json.Marshal(payload)
+
+		req, err := http.NewRequest("POST", "/login", bytes.NewBuffer(body))
+		require.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, req)
+
+		require.Equal(t, http.StatusUnauthorized, recorder.Code)
+	})
+
+	t.Run("Test incorrect password", func(t *testing.T) {
+		mockUser, err := domain.NewUser(
+			uid,
+			"Jean Luc Picard",
+			email,
+			"enterprise",
+		)
+
+		repo.On("FindByEmail", "jlp@federation.com").Return(mockUser, nil)
+
+		payload := map[string]string{
+			"email":    "jlp@federation.com",
+			"password": "ferengi",
+		}
+
+		body, _ := json.Marshal(payload)
+
+		req, err := http.NewRequest("POST", "/login", bytes.NewBuffer(body))
+		require.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, req)
+
+		assert.Equal(t, http.StatusUnauthorized, recorder.Code)
+	})
 }
